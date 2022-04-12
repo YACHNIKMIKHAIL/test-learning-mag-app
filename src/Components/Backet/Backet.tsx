f352fbe6359e40342e4f9f996771242cb163c093
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-04-12 21:47:32 +0300

    All local storage work! commit

--- src/Components/Backet/Backet.tsx
+++ src/Components/Backet/Backet.tsx
@@ -1,23 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Order from "../Order/Order";
 import ByedItem from "../ByedItem/ByedItem";
 import {BacketCase, ByedItemsCase, OrderCase} from './BacketStyles';
 import {useMagSelector} from "../../App/store";
 import {ItemsType} from "../../Api/MagAPI";
-import {useDispatch} from "react-redux";
 
 const Backet = () => {
     const byedItems=useMagSelector<ItemsType[]>(state=>state.items.byedItems.bItems)
-
-    const [byedI, setByedI] = useState<ItemsType[]>(byedItems)
-
-    // useEffect(() => {
-    //     let result = localStorage.getItem('itemsInBacket')
-    //     if (result !== null) {
-    //         setByedI(JSON.parse(result))
-    //     }
-    // }, [])
-
 
     return (
         <BacketCase>
