5d51b8a9209f895fd8732dba546e03ee6df3f8fc
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-04-14 10:28:23 +0300

    start validate commit

--- src/Components/Order/Order.tsx
+++ src/Components/Order/Order.tsx
@@ -1,14 +1,86 @@
 import React from 'react';
-import {MainOrderCase} from './OrderStyles';
+import {FormCase, MainOrderCase} from './OrderStyles';
+import {useFormik} from "formik";
 
 const Order = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            surname: '',
+            email: '',
+            country: '',
+            city: '',
+            street: '',
+        },
+        onSubmit: value => {
+            console.log({
+                name: value.name,
+                surname: value.surname,
+                email: value.email,
+                country: value.country,
+                city: value.city,
+                street: value.street,
+            })
+            formik.resetForm()
+            // axios.post("https://ymndjs.herokuapp.com/sendMessage", {
+            //     // axios.post("http://localhost:3010/sendMessage", {
+            //     name: value.name,
+            //     surname: value.surname,
+            //     email: value.email,
+            //     address: value.address,
+            // })
+            //     .then(() => {
+            //         alert('ok!')
+            //     })
+        }
+    })
+
     return (
         <MainOrderCase>
-            <input type="text" placeholder={'name'}/>
-            <input type="text" placeholder={'surname'}/>
-            <input type="text" placeholder={'address'}/>
-            <input type="text" placeholder={'phone'}/>
-            <button>ORDER</button>
+            <form onSubmit={formik.handleSubmit}>
+                <FormCase>
+                    Name: <input
+                    type={"text"}
+                    placeholder={"Name"}
+                    {...formik.getFieldProps('name')}
+                />
+
+                {/*<input type="text" placeholder={'surname'}/>*/}
+                    Surname: <input
+                    type={"text"}
+                    placeholder={"Surname"}
+                    {...formik.getFieldProps('surname')}
+                />
+
+                {/*<input type="text" placeholder={'phone'}/>*/}
+                    Email: <input
+                    type={"text"}
+                    placeholder={"Email"}
+                    {...formik.getFieldProps('email')}
+                />
+
+                {/*<input type="text" placeholder={'address'}/>*/}
+                    Address: <input
+                    type={"text"}
+                    placeholder={"Country"}
+                    {...formik.getFieldProps('country')}
+                />
+                    <input
+                        type={"text"}
+                        placeholder={"City"}
+                        {...formik.getFieldProps('city')}
+                    />
+                    <input
+                        type={"text"}
+                        placeholder={"Street"}
+                        {...formik.getFieldProps('street')}
+                    />
+
+                <button type={'submit'}>ORDER</button>
+
+                {/*<input type="text" placeholder={'name'}/>*/}
+                </FormCase>
+            </form>
         </MainOrderCase>
     );
 };
--- src/Components/Order/OrderStyles.ts
+++ src/Components/Order/OrderStyles.ts
@@ -11,3 +11,13 @@ export const MainOrderCase = styled.div`
   background-color: rgba(115, 77, 230, 0.51);
   border: 2px aqua solid;
 `
+export const FormCase = styled.div`
+  height: 300px;
+  min-width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 20px;
+  //background-color: rgba(115, 77, 230, 0.72);
+`
